b0321c2b2dbecaa26e1686a0479b3891c3e2b357
Himani Acharya <himaniacharya1@gmail.com>
2023-11-05 09:52:11 +0545

    first commit

---
+++ healthsync/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './screens/Home';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home/>
+      </div>
+    </>
+  );
+}
+
+export default App;
