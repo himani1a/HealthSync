92fb1d1304ced2f899bc022443b3702dec07cf9f
Himani Acharya <himaniacharya1@gmail.com>
2024-02-02 14:00:07 +0545

    commit

--- server/controller/appController.js
+++ server/controller/appController.js
@@ -34,72 +34,126 @@ export async function verifyUser(req, res, next){
     }
 }
 
+/** POST: http://localhost:8080/api/signup
+ * @param : {
+  "username" : "example123",
+  "password" : "admin123",
+  "email": "example@gmail.com",
+  "firstName" : "bill",
+  "lastName": "william",
+  "mobile": 8009860560,
+  "address" : "Apt. 556, Kulas Light, Gwenborough",
+  "profile": ""
+}
+*/
+// export async function signup(req, res) {
+
+//     try {
+//         const { username, password, profile, email } = req.body;
+
+//         // check the existing user
+//         const existUsername =  new Promise((resolve, reject) => {
+//             UserModel.findOne({ username }, function (err, user) {
+//                 if (err) reject(new Error(err))
+//                 if (user) reject({ error: "Please use unique username" });
+
+//                 resolve();
+//             })
+//         });
+
+//         // check for existing email
+//         const existEmail = new Promise((resolve, reject) => {
+//             UserModel.findOne({ email }, function (err, email) {
+//                 if (err) reject(new Error(err))
+//                 if (email) reject({ error: "Please use unique Email" });
+
+//                 resolve();
+//             })
+//         });
+
+
+//         Promise.all([existUsername, existEmail])
+//             .then(() => {
+//                 if (password) {
+//                     bcrypt.hash(password, 10)
+//                         .then(hashedPassword => {
+
+//                             const user = new UserModel({
+//                                 username,
+//                                 password: hashedPassword,
+//                                 profile: profile || '',
+//                                 email
+//                             });
+
+//                             // return save result as a response
+//                             user.save()
+//                                 .then(result => res.status(201).send({ msg: "User Register Successfully" }))
+//                                 .catch(error => res.status(500).send({ error}))
+
+//                         }).catch(error => {
+//                             return res.status(500).send({
+//                                 error: "Enable to hashed password"
+//                             })
+//                         })
+//                 }
+//             }).catch(error => {
+//                 return res.status(500).send({ error })
+//             })
+
+
+//     } catch (error) {
+//         return res.status(500).send(error);
+//     }
+
+// }
+
 
 export async function signup(req, res) {
-
     try {
         const { username, password, profile, email } = req.body;
 
-        // check the existing user
-        const existUsername =  new Promise((resolve, reject) => {
-            UserModel.findOne({ username }, function (err, user) {
-                if (err) reject(new Error(err))
-                if (user) reject({ error: "Please use unique username" });
+        // Check for existing user by username
+        const existingUser = await UserModel.findOne({ username });
+        if (existingUser) {
+            return res.status(400).send({ error: "Please use a unique username." });
+        }
 
-                resolve();
-            })
-        });
+        // Check for existing user by email
+        const existingEmail = await UserModel.findOne({ email });
+        if (existingEmail) {
+            return res.status(400).send({ error: "Please use a unique email." });
+        }
 
-        // check for existing email
-        const existEmail = new Promise((resolve, reject) => {
-            UserModel.findOne({ email }, function (err, email) {
-                if (err) reject(new Error(err))
-                if (email) reject({ error: "Please use unique Email" });
+        // Proceed with password hashing and user creation if no conflicts
+        if (password) {
+            try {
+                const hashedPassword = await bcrypt.hash(password, 10);
+                const user = new UserModel({
+                    username,
+                    password: hashedPassword,
+                    profile: profile || '',
+                    email
+                });
 
-                resolve();
-            })
-        });
-
-
-        Promise.all([existUsername, existEmail])
-            .then(() => {
-                if (password) {
-                    bcrypt.hash(password, 10)
-                        .then(hashedPassword => {
-
-                            const user = new UserModel({
-                                username,
-                                password: hashedPassword,
-                                profile: profile || '',
-                                email
-                            });
-
-                            // return save result as a response
-                            user.save()
-                                .then(result => res.status(201).send({ msg: "User Register Successfully" }))
-                                .catch(error => res.status(500).send({ error}))
-
-                        }).catch(error => {
-                            return res.status(500).send({
-                                error: "Enable to hashed password"
-                            })
-                        })
-                }
-            }).catch(error => {
-                return res.status(500).send({ error })
-            })
-
-
+                // Save the user and return success response
+                await user.save();
+                return res.status(201).send({ msg: "User registered successfully." });
+            } catch (error) {
+                // Error in password hashing
+                return res.status(500).send({ error: "Unable to hash password." });
+            }
+        } else {
+            // Password not provided
+            return res.status(400).send({ error: "Password is required." });
+        }
     } catch (error) {
-        return res.status(500).send(error);
+        // Catch any other unexpected errors
+        return res.status(500).send({ error: error.message || "An unexpected error occurred." });
     }
-
 }
 
 
-
-
-/** POST: http://localhost:8000/api/login 
+/** POST: http://localhost:8000/api/signup
  * @param: {
   "username" : "example123",
   "password" : "admin123"
